c251b3c050b94872e40b8a0e8a484d5e7f075c97
ife0ye <110231746+ife0ye@users.noreply.github.com>
2023-10-27 22:22:21 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const app = express();
+const cors = require('cors');
 const port = 3001;
 
 app.use(express.json());
